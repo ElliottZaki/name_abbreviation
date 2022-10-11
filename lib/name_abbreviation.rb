66f1ebe06206c859d5f910449e9f6a4f917b55e7
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-11 14:33:48 +0100

    Fourth commit. Testes passing. Created a Capitalize method which as it implies capitalizes both initals.

--- lib/name_abbreviation.rb
+++ lib/name_abbreviation.rb
@@ -1,18 +1,22 @@
 class Name_abbreviation
+  name = 'jane doe'
 
-    name = "jane doe"
+  def start_app(name)
+    array = transform_string_to_array(name)
+    initals = find_inital(array)
+    capitalize(initals)
+  end
 
-    def start_app(name)
-        array = transform_string_to_array(name)
-        initals = find_inital(array)
-    end
 
-    def transform_string_to_array(name)
-        array = name.split(' ')
-    end 
+  def transform_string_to_array(name)
+    array = name.split(' ')
+  end
 
-    def find_initial(array)
-        initals =+ (array[0][0] + "." + array[1][0])
-    end
+  def find_initial(array)
+    initals = + (array[0][0] + '.' + array[1][0])
+  end
 
+  def capitalize(initals)
+    initals.upcase
+  end
 end
--- spec/name_abbreviation_spec.rb
+++ spec/name_abbreviation_spec.rb
@@ -1,18 +1,23 @@
-require './lib/name_abbreviation.rb'
+require './lib/name_abbreviation'
 
 describe Name_abbreviation do
-    let (:name_abbreviation) {double :name_abbreviation }
+  let(:name_abbreviation) { double :name_abbreviation }
 
-    describe '#transform_string_to_array' do
-        it 'should transform a string into an array' do
-            expect(subject.transform_string_to_array("john doe")).to eq(["john", "doe"]) 
-        end
+  describe '#transform_string_to_array' do
+    it 'should transform a string into an array' do
+      expect(subject.transform_string_to_array('john doe')).to eq(%w[john doe])
     end
-    
-    describe '#find_inital' do
-        it 'should find the initials of the first & second name' do
-            expect(subject.find_initial(["john", "doe"])).to eq("j.d") 
-        end
-    end
+  end
 
+  describe '#find_inital' do
+    it 'should find the initials of the first & second name' do
+      expect(subject.find_initial(%w[john doe])).to eq('j.d')
+    end
+  end
+
+  describe '#capitalize' do
+    it 'should capitalize the initials' do
+      expect(subject.capitalize('j.d')).to eq('J.D')
+    end
+  end
 end
