bf72d5c43619554b138b28cbede03faf65d0099c
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-11 14:18:54 +0100

    Third commit. Second passing test. Created a method inwhich it pulls the initials from an array by selecting the [0] element of the first word and the [0] element of the second word.

--- lib/name_abbreviation.rb
+++ lib/name_abbreviation.rb
@@ -1,10 +1,18 @@
 class Name_abbreviation
 
-name = "john"
+    name = "jane doe"
+
+    def start_app(name)
+        array = transform_string_to_array(name)
+        initals = find_inital(array)
+    end
 
     def transform_string_to_array(name)
-        array = name.split('')
-        p array
+        array = name.split(' ')
+    end 
+
+    def find_initial(array)
+        initals =+ (array[0][0] + "." + array[1][0])
     end
 
 end
--- spec/name_abbreviation_spec.rb
+++ spec/name_abbreviation_spec.rb
@@ -1,10 +1,17 @@
 require './lib/name_abbreviation.rb'
 
 describe Name_abbreviation do
+    let (:name_abbreviation) {double :name_abbreviation }
 
     describe '#transform_string_to_array' do
-        it 'should transform a string into an array, with each letter being a different element' do
-            expect(subject.transform_string_to_array("john")).to eq(["j", "o", "h", "n"]) 
+        it 'should transform a string into an array' do
+            expect(subject.transform_string_to_array("john doe")).to eq(["john", "doe"]) 
+        end
+    end
+    
+    describe '#find_inital' do
+        it 'should find the initials of the first & second name' do
+            expect(subject.find_initial(["john", "doe"])).to eq("j.d") 
         end
     end
 
